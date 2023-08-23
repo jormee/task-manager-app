b3659619df6643d0215fd795c70f4042921e1e39
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2023-08-23 15:13:07 +0100

    Connect app to database

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 const port = process.env.port || 5000
 
 app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 
-app.listen(port, () =>{
-  console.log(`app is running on port ${port}...`)
-})
+const startServer = async () => {
+  try{
+    await connectDB().then(() => console.log("Connected to DB"))
+    app.listen(port, () => {
+      console.log(`app is running on port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startServer()
